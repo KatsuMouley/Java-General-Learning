196eac7f8b42ead59a961917c330b6596777c9bf
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-09 07:43:13 -0300

    Convertion, GUI, math, scanner exercises

---
+++ conversao.java
@@ -0,0 +1,14 @@
+public class conversao {
+    public static void main(String[] args) {
+        // Contas matemáticas em java não tem tanta diferença do básico. / % - + continuam sendo utilizados normalmente.
+        double Decimals = 15.6;
+        int Ints = 10;
+        Decimals = (double) Ints / 3;// Converção de valores
+        String linha = "look at this " + Decimals + " number.";
+        System.out.println(linha); 
+        //Oh no our double number output is too long! how can we solve this?
+        //How about this?
+        String FormatedNumber = String.format( "%.2f", Decimals);
+        System.out.println("No! Instead look at this " + FormatedNumber +" number"); 
+    }
+}
