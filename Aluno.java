2b443e2081a1df1c21017ad773e20c65d8fc47e8
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-11 21:18:04 -0300

    Flags, Final keyword and basic printf Exercises

--- Aluno.java
+++
@@ -1,9 +0,0 @@
-public class Aluno{
-    public static void main(){
-        String matricula;
-        String nome;
-        int.nota1;
-        int.nota2
-        
-    }
-}
--- printf.java
+++ printf.java
@@ -1,4 +1,4 @@
-public class printf(){
+public class printf{
 
     public static void main(){
         boolean myBoolean = true;
