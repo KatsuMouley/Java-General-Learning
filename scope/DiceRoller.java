9f021a83541679e73e3f1c86eaf581d5e9f49397
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-04-12 07:43:21 -0300

    Scope

--- scope/DiceRoller.java
+++ scope/DiceRoller.java
@@ -1,5 +1,17 @@
 package scope;
-
+import java.util.Random;
 public class DiceRoller {
-    
+
+    Random random1;
+    int number1;
+
+    DiceRoller(){
+        random1 = new Random();
+        roll();
+    }
+
+    void Roll(){
+        number1 = random(6)+1;
+        System.out.println(number1);
+    }
 }
--- scope/Main.java
+++ scope/Main.java
@@ -1,5 +1,8 @@
 package scope;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        DiceRoller diceRoller = new DiceRoller();
+        
+    }
 }
